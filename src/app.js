5fe5b5da44cf9bb05249f09a9d0d9c1435cc4677
Levon Stepanyan <levonmll@gmail.com>
2023-01-26 00:22:54 +0200

    uibuild

--- src/app.js
+++ src/app.js
@@ -15,7 +15,7 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 const app = express()
-
+mongoose.set('strictQuery', true)
 mongoose
   .connect(config.MONGODB_URI)
   .then(() => logger.info('connected to database'))
