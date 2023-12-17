404bf295e22ee98c4f72bafee06a90bef46d6356
Levon Stepanyan <levonmll@gmail.com>
2023-12-17 19:14:54 +0200

    add ahrefs verification file to  the root

--- src/app.js
+++ src/app.js
@@ -1,39 +1,38 @@
-import cors from 'cors'
-import express from 'express'
-import mongoose from 'mongoose'
-import path from 'path'
-import { fileURLToPath } from 'url'
-import contentRouter from './controllers/contentRouter.js'
-import loginRouter from './controllers/loginRouter.js'
-import signupRouter from './controllers/signupRouter.js'
-import userRouter from './controllers/userRouter.js'
-import config from './utils/config.js'
-import logger from './utils/logger.js'
-import middleware from './utils/middlewares.js'
+import cors from "cors"
+import express from "express"
+import mongoose from "mongoose"
+import path from "path"
+import { fileURLToPath } from "url"
+import contentRouter from "./controllers/contentRouter.js"
+import loginRouter from "./controllers/loginRouter.js"
+import signupRouter from "./controllers/signupRouter.js"
+import userRouter from "./controllers/userRouter.js"
+import config from "./utils/config.js"
+import logger from "./utils/logger.js"
+import middleware from "./utils/middlewares.js"
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 const app = express()
-mongoose.set('strictQuery', true)
+//console.log("-----mngdburi", config.MONGODB_URI)
+mongoose.set("strictQuery", true)
 mongoose
-  .connect(config.MONGODB_URI)
-  .then(() => logger.info('connected to database'))
-  .catch((error) =>
-    logger.error('error connecting to database: ', error.message)
-  )
+	.connect(config.MONGODB_URI)
+	.then(() => logger.info("connected to database"))
+	.catch((error) => logger.error("error connecting to database: ", error))
 app.use(cors())
 app.use(express.json())
-app.use('/api/login', loginRouter)
-app.use('/api/users', userRouter)
-app.use('/api/signup', signupRouter)
-app.use('/api/content', contentRouter)
-app.use(express.static('./build'))
-const isProduction = process.env.NODE_ENV === 'production'
+app.use("/api/login", loginRouter)
+app.use("/api/users", userRouter)
+app.use("/api/signup", signupRouter)
+app.use("/api/content", contentRouter)
+app.use(express.static("./build"))
+const isProduction = process.env.NODE_ENV === "production"
 isProduction &&
-  app.get('*', function (request, response) {
-    response.sendFile(path.resolve(__dirname, '../build', 'index.html'))
-  })
+	app.get("*", function (request, response) {
+		response.sendFile(path.resolve(__dirname, "../build", "index.html"))
+	})
 app.use(middleware.errorHandler)
 app.use(middleware.unknownEndpoint)
 
